b958defd48b02fa6209868a87875f286c93ddceb
Josh W <wilson.jd022@gmail.com>
2024-06-04 12:58:59 -0700

    game camera has a lower y limit and x-axis limits

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -3,9 +3,21 @@ class_name Game_Camera
 extends Camera2D
 
 var player: Player
+var viewport_size: Vector2;
 
 func _ready() -> void:
-	global_position.x = get_viewport_rect().size.x / 2
+	viewport_size = get_viewport_rect().size
+	global_position.x = viewport_size.x / 2
+	
+	limit_bottom = viewport_size.y
+	limit_left = 0
+	limit_right = viewport_size.x
+	
+func _process(delta: float) -> void:
+	if player:
+		var limit_offset = 420
+		if limit_bottom > player.global_position.y + limit_offset:
+			limit_bottom = player.global_position.y + limit_offset
 	
 func _physics_process(delta: float) -> void:
 	if player != null:
